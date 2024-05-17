cbad4c0b754b8305a0a685e030348ae0ba6c8d2c
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-17 19:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function mincost(arr)
 { 
+	let sum=0;
+	arr=arr.sort()
+	const arr2= arr.map((e)=>{
+		return sum+=e;
+	})
+	sum=0;
+	arr2.map((e)=>{
+		sum+=e;
+	})
+	return sum
+	
+	
 //write your code here
 // return the min cost
   
